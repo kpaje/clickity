8463d1cbf10005da35f1d3f4aecf4a2128739ef5
kpaje <kelvinmembership@gmail.com>
2019-02-05 16:20:21 -0800

    the wire setup

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron mt-4">
+      THE WIRE
+      <p>Choose your allies wisely!</p>
+    </div>
+  );
+}
+
+export default Jumbotron;
